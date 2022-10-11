90fa06094ca157d160dcbdb4a86204bc10ed21d4
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-11 14:31:38 +0200

    Tag List API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -15,3 +18,10 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + [
             'description',
         ]
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
