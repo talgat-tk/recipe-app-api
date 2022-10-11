13db255dca4eccb1bf34f15c50d5fa1a8e412894
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-11 12:49:57 +0200

    Recipe Detail API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,3 +8,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + [
+            'description',
+        ]
