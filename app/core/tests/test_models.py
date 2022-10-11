06d61f677fd6e8a34fcea9da41a69c712a449d87
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-11 14:14:48 +0200

    Create Tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,10 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """Test models."""
 
@@ -66,3 +70,9 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
