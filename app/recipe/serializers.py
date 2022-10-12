893d129e98038afd900bbcc84638726337a1c0ac
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-12 12:22:44 +0200

    Upload Image API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -80,5 +80,13 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + [
-            'description',
+            'description', 'image',
         ]
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
