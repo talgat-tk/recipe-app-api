f8a2cc97bf9e928d7eacc44b0f2b388820f74f72
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-12 11:57:53 +0200

    Filename generating function

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
 """
 Database models.
 """
+import uuid
+import os
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -8,6 +11,13 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
+
+
+def recipe_image_file_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
